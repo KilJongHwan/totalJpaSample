369eabf4f6c95d4bdea2024850f32e5829708d4e
JH <dafr47@naver.com>
2023-11-22 12:02:55 +0900

    order, 스웨거 추가

---
+++ src/main/java/com/kh/totaljpaSample/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.kh.totaljpaSample.constant;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
--- src/main/java/com/kh/totaljpaSample/entity/CartItem.java
+++ src/main/java/com/kh/totaljpaSample/entity/CartItem.java
@@ -15,7 +15,13 @@ public class CartItem {
     @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne  // 하나의 장바구니에는 여러개의 상품을 담을 수 있음 (다대일 관계)
     @JoinColumn(name = "cart_id")
     private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;  // 하나의 아이템은 여러 장바구니에 상품으로 담길 수 있음(다대일 관계)
+
+    private int count;
 }
